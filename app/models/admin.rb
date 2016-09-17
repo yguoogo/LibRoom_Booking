7d76c198891f477f96c0124fa1ef4aca7ee23a6b
yguoogo <ygxoogo@gmail.com>
2016-09-17 13:00:48 -0400

    Test the validation of Admins Model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,10 @@
 class Admin < ApplicationRecord
+  VALNAME = /[0-9a-z_]{6,12}/i
+  validates :name,  presence: true,length: {maximum: 12 },
+             format: { with: VALNAME }
+
+  VALEMAIL = /[0-9a-z]*@[0-9a-z]*\.[0-9a-z]*/i
+  validates :email, presence: true, length: { maximum: 256 },
+            format: { with: VALEMAIL }
+  validates :password, presence: true
 end
--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -9,8 +9,45 @@ class AdminTest < ActiveSupport::TestCase
     assert @admin.valid?
   end
 
+  test "admin name can not be blank" do
+    @admin.name = "         "
+    assert_not @admin.valid?
+  end
+
+  test "admin email can not be blank" do
+    @admin.email = "                  "
+    assert_not @admin.valid?
+  end
+
+  test "admin password can not be blank" do
+    @admin.password = "                     "
+    assert_not @admin.valid?
+  end
+
+  test "admin name can not be longer than 12" do
+    @test_name = "1"*13
+    @admin.name = @test_name
+    assert_not @admin.valid?
+  end
+
+  test "admin email can not be longer than 256" do
+    @test_email = "1"*257
+    @admin.email = @test_email
+    assert_not @admin.valid?
+  end
+
+  test "admin name format" do
+    @admin.name = "haha*()_"
+    assert_not @admin.valid?
+  end
+
+  test "admin email format" do
+    @admin.email = "ahweoohigr"
+    assert_not @admin.valid?
+  end
+
   def setup
-    @admin = Admin.new(name:"temporary name",email:"123456@gmail.com",password:"123456")
+    @admin = Admin.new(name:"tem_name",email:"123456@gmail.com",password:"123456")
   end
 
 end
